c32ad0e863f59f8d99a137de2bf9481aaad1edef
benjamin <benjamin.michel.cn@gmail.com>
2012-09-02 15:53:55 -0500

    Changed the database connection to ORM with idiorm
    Now using idiorm for database connection
    Updated the web services.

--- server/index.php
+++ server/index.php
@@ -11,6 +11,9 @@
 require 'Slim/Slim.php';
 $app = new Slim();
 
+// Load idiorm
+require 'idiorm.php';
+
 // Database connection with middleware
 class DBConnectionMiddleware extends Slim_Middleware{
 
@@ -18,9 +21,9 @@ class DBConnectionMiddleware extends Slim_Middleware{
 	public function call(){
 
 		// Initialize the database
-		$this->app->db = mysql_connect('localhost', 'root', ''); 
-		if (!mysql_select_db('phonebook', $this->app->db))
-			throw new Exception('Cannot open the "phonebook" database');
+		ORM::configure('mysql:host=localhost;dbname=phonebook');
+		ORM::configure('username', 'root');
+		ORM::configure('password', '');
 
 		// Execute the application
 		$this->next->call();
@@ -28,8 +31,6 @@ class DBConnectionMiddleware extends Slim_Middleware{
 		//set content type
 		$response = $this->app->response();
 		$response['Content-Type'] = 'application/json';
-
-		mysql_close($this->app->db);
 	}
 }
 // Add middleware to app
@@ -54,71 +55,76 @@ $app->add(new DBConnectionMiddleware());
 
 // List
 $app->get('/contacts/', function () use ($app) {
-	$sql = 'SELECT * FROM Contact';
-	$qry = mysql_query($sql, $app->db);
-	$res = array();
-	while ($row = mysql_fetch_object($qry))
-		$res[] = $row;
+	// Get the contacts
+	$contacts = ORM::for_table('contact')->find_many();
 
+	// Put in array
+	$res = array();
+	foreach($contacts as $contact)
+		$res[] = $contact->as_array();
+	 
 	$app->response()->status(200);
 	$app->response()->body(json_encode($res));
 });
 
 // Create
 $app->post('/contacts/', function () use ($app) {
+	// Get the params
 	$params = json_decode($app->request()->getBody(), true);
-	$sql = 'INSERT INTO Contact (first_name, last_name) VALUES ("'
-		. $params['first_name'] . '", "'
-		. $params['last_name'] . '")';
 
-	if (mysql_query($sql, $app->db)) { // Success
+	// Create contact
+	$contact = ORM::for_table('contact')->create();
+	$contact->first_name = $params['first_name'];
+	$contact->last_name = $params['last_name'];
+	$contact->save();
+
+	if ($contact->id()) { // Success
 		// Give the Contact's data
-		$app->redirect(mysql_insert_id());
+		$app->redirect($contact->id());
 	} else { // Failure
 		$app->response()->status(404);
-		$app->response()->body(mysql_error());
 	}
 });
 
 // Update
 $app->put('/contacts/:id', function($id) use ($app) {
+	// Get the params
 	$params = json_decode($app->request()->getBody(), true);
-	$sql = 'UPDATE Contact SET '
-		. 'first_name = "' . $params['first_name'] . '", '
-		. 'last_name = "' . $params['last_name'] . '" '
-		. 'WHERE id=' . $id;
 
-	if (mysql_query($sql, $app->db)) { // Success
+	// Update contact
+	$contact = ORM::for_table('contact')->find_one($id);
+	$contact->first_name = $params['first_name'];
+	$contact->last_name = $params['last_name'];
+	$contact->save();
+
+	if ($contact->id()) { // Success
 		$app->response()->status(200);
 	} else { // Failure
 		$app->response()->status(404);
-		$app->response()->body(mysql_error());
 	}
 });
 
 // Destroy
 $app->delete('/contacts/:id', function ($id) use ($app) {
-	$sql = 'DELETE FROM Contact WHERE id = ' . $id;
 
-	if (mysql_query($sql, $app->db)) { // Success
-		$app->response()->status(200);
-	} else { // Failure
-		$app->response()->status(404);
-		$app->response()->body(mysql_error());
-	}
+	// Delete the contact
+	$contact = ORM::for_table('contact')->find_one($id);
+	$contact->delete();
+
+	$app->response()->status(200);
 });
 
 // Item
 $app->get('/contacts/:id', function($id) use ($app) {
-	$sql = 'SELECT * FROM Contact WHERE id = ' . $id;
-	$qry = mysql_query($sql, $app->db);
 
-	if ($item = mysql_fetch_object($qry)) { // Success
+	// Get the contact
+	$contact = ORM::for_table('contact')->find_one($id);
+
+	if ($contact) { // Success
 		$app->response()->status(200);
-		$app->response()->body(json_encode($item));
+		$app->response()->body(json_encode($contact->as_array()));
 	} else { // Failure
 		$app->response()->status(404);
-		$app->response()->body(mysql_error());
 	}
 });
 
@@ -128,11 +134,16 @@ $app->get('/contacts/:id', function($id) use ($app) {
 
 // List
 $app->get('/contacts/:contact_id/phones/', function ($contact_id) use ($app) {
-	$sql = 'SELECT * FROM Phone WHERE contact_id = ' . $contact_id;
-	$qry = mysql_query($sql, $app->db);
+
+	// Find the phones
+	$phones = ORM::for_table('phone')
+		->where('contact_id', $contact_id)
+		->find_many();
+
+	// Put in array
 	$res = array();
-	while ($row = mysql_fetch_object($qry))
-		$res[] = $row;
+	foreach($phones as $phone)
+		$res[] = $phone->as_array();
 
 	$app->response()->status(200);
 	$app->response()->body(json_encode($res));
@@ -140,61 +151,64 @@ $app->get('/contacts/:contact_id/phones/', function ($contact_id) use ($app) {
 
 // Create
 $app->post('/contacts/:contact_id/phones/', function ($contact_id) use ($app) {
-	$app->response()->status(200);
+	// Get parameters
 	$params = json_decode($app->request()->getBody(), true);
-	$sql = 'INSERT INTO Phone (contact_id, number, type) VALUES ('
-		. $params['contact_id'] . ', "'
-		. $params['number'] . '", "'
-		. $params['type'] . '")';
 
-	if (mysql_query($sql, $app->db)) { // Success
+	// Create Phone
+	$phone = ORM::for_table('phone')->create();
+	$phone->contact_id = $params['contact_id'];
+	$phone->number = $params['number'];
+	$phone->type = $params['type'];
+	$phone->save();
+
+	if ($phone->id()) { // Success
 		// Give the Phone's data
-		$app->redirect(mysql_insert_id());
+		$app->response()->status(200);
+		$app->redirect($phone->id());
 	} else { // Failure
 		$app->response()->status(404);
-		$app->response()->body(mysql_error());
 	}
 });
 
 //Update
 $app->put('/contacts/:contact_id/phones/:id', function($contact_id, $id) use ($app) {
+	// Get parameters
 	$params = json_decode($app->request()->getBody(), true);
-	$sql = 'UPDATE Phone SET '
-		. 'number = "' . $params['number'] . '", '
-		. 'type = "' . $params['type'] . '" '
-		. 'WHERE id=' . $id;
 
-	if (mysql_query($sql, $app->db)) { // Success
+	// Update the data
+	$phone = ORM::for_table('phone')->find_one($id);
+	$phone->number = $params['number'];
+	$phone->type = $params['type'];
+	$phone->save();
+
+	if ($phone->id()) { // Success
 		$app->response()->status(200);
 	} else { // Failure
 		$app->response()->status(404);
-		$app->response()->body(mysql_error());
 	}
 });
 
 //Destroy
 $app->delete('/contacts/:contact_id/phones/:id', function ($contact_id, $id) use ($app) {
-	$sql = 'DELETE FROM Phone WHERE id = ' . $id;
+	
+	// Delete
+	$phone = ORM::for_table('phone')->find_one($id);
+	$phone->delete();
 
-	if (mysql_query($sql, $app->db)) { // Success
-		$app->response()->status(200);
-	} else { // Failure
-		$app->response()->status(404);
-		$app->response()->body(mysql_error());
-	}
+	$app->response()->status(200);
 });
 
 // Item
 $app->get('/contacts/:contact_id/phones/:id', function($contact_id, $id) use ($app) {
-	$sql = 'SELECT * FROM Phone WHERE id = ' . $id;
-	$qry = mysql_query($sql, $app->db);
 
-	if ($item = mysql_fetch_object($qry)) { // Success
+	// Get the phone
+	$phone = ORM::for_table('phone')->find_one($id);
+
+	if ($phone) { // Success
 		$app->response()->status(200);
-		$app->response()->body(json_encode($item));
+		$app->response()->body(json_encode($phone->as_array()));
 	} else { // Failure
 		$app->response()->status(404);
-		$app->response()->body(mysql_error());
 	}
 });
 
